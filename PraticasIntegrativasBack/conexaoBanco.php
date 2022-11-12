40f89a6a4cfd55495d33e2015b221035da592955
felippefsantana <felippe.santana@fabricainfo.com>
2022-11-12 11:49:33 -0300

    estrutura de pastas

--- PraticasIntegrativasBack/conexaoBanco.php
+++
@@ -1,4 +0,0 @@
-<!-- EXEMPLO: -->
-<?php 
-	$conn = mysqli_connect("localhost", "root", "", "republica") or die('Não foi possível conectar');
-?>
---
+++ src/database/connection.php
@@ -0,0 +1,4 @@
+<!-- EXEMPLO: -->
+<?php 
+	$conn = mysqli_connect("localhost", "root", "", "ks") or die('Não foi possível conectar');
+?>
