4b8bd90eecd042bcd85fb7e14a17e29217a0764f
felippefsantana <felippe.santana@fabricainfo.com>
2022-11-14 16:10:08 -0300

    resolving bug when get the uri

--- index.php
+++ index.php
@@ -6,4 +6,64 @@ use App\Common\Environment;
 
 Environment::load(__DIR__);
 
-$env = getenv();
+// $env = getenv();
+// echo '<pre>';
+// print_r($_SERVER);
+// echo '</pre>';
+// exit;
+
+$routesCollection = [
+  "get" => [
+    "/",
+    "/alunos",
+    "/despesas"
+  ],
+  "post" => [
+    "/alunos",
+    "/despesas"
+  ],
+  "put" => [
+    "/alunos",
+    "/despesas"
+  ],
+  "delete" => [
+    "/alunos",
+    "/despesas"
+  ]
+];
+
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+$uri = $_SERVER['REQUEST_URI'];
+
+switch($method) {
+  case 'get':
+    if(!$routesCollection[$method][array_search($uri, $routesCollection)]) {
+      return false;
+    }
+    echo 'é um get';
+    break;
+
+  case 'post':
+    if(!$routesCollection[$method][array_search($uri, $routesCollection)]) {
+      return false;
+    }
+    echo 'é um post';
+    break;
+
+  case 'put':
+    if(!$routesCollection[$method][array_search($uri, $routesCollection)]) {
+      return false;
+    }
+    echo 'é um put';
+    break;
+
+  case 'delete':
+    if(!$routesCollection[$method][array_search($uri, $routesCollection)]) {
+      return false;
+    }
+    echo 'é um delete';
+    break;
+
+  default:
+    break;
+}
